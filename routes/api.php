d60211f2f384ac9fea2cb88e7960df232036f83a
willywin99 <willywinsianturi@gmail.com>
2022-01-01 08:10:07 +0700

    Autentikasi API dengan Passport - Login, Register, Get Profile

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,13 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', 'API\UserController@register');
+Route::post('login', 'API\UserController@login');
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::middleware('auth:api')->group(function () {
+    Route::get('/profile', 'API\UserController@profile');
 });
+
